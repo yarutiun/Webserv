1e1a871fb6b33c2682b782c5e291e0158b13270c
Santiago Tena Hernandez <stenahe@gmail.com>
2023-10-21 20:45:58 +0200

    Add files

--- Configuration/ConfigFileParser.hpp
+++ Configuration/ConfigFileParser.hpp
@@ -4,7 +4,18 @@
 #include "../webserv.hpp"
 
 class ConfigFileParser {
+private:
+    std::vector<Configuration>     _configs_;
 
+    std::string                     _loadFile(const char *file);
+    std::string                     _getConfigElement(std::string &element);
+    bool                            _combineSharedNetAddress(const Configuration &config, size_t size);
+    bool                            _sharedNetAddress(const Configuration &configOne, const Configuration &configTwo);
+
+public:
+    ConfigFileParser(const char *configFile);
+
+    std::vector<Configuration>     getConfigs() const;
 };
 
 #endif
